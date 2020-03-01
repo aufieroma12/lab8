944198c4464ece8cbcead5b5b785df7fc18dfcd7
Massimo Aufiero <Massimo@dhcp-10-250-24-0.harvard.edu>
2020-03-01 12:50:32 -0500

    real final?

--- lab8_part2.ml
+++ lab8_part2.ml
@@ -163,6 +163,6 @@ the string will be made up of alphanumeric characters only.
 module IntStringStack = MakeStack (struct
                                     type t = (int * string)
                                     let serialize = fun (x : t) : string ->
-                                    let (n, s) = x in "(" ^ string_of_int n ^ ", " ^ s ^ ")"  end) ;;
+                                    let (n, s) = x in "(" ^ string_of_int n ^ "," ^ s ^ ")"  end) ;;
 
 
